615302e7e73624cc97524d8eaecd236800201948
Rumesa <fatimarumesa2@gmail.com>
2026-04-17 21:41:08 +0530

    Add variables for VM user and password

---
+++ var.tf
@@ -0,0 +1,17 @@
+#variable values
+variable "vm_user" {
+  type    = string
+  default = "Rumesa"
+}
+variable "vm_pass" {
+  type    = string
+  default = "pass@1234"
+}
+variable "usernameb" {
+    type = string
+    default = "Rumesa"
+  }
+variable "passb" {
+     type = string
+     default = "pass@1234"
+}
